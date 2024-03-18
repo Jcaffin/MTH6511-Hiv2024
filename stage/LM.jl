de394f8b9697856ef452180d6dcb3267a8f33e3f
Jcaffin <jules.caffin@gmail.com>
2024-03-18 16:07:00 -0400

    LM_D : rho adapté au modèle avec D, lambda à la place de lambda^2 et calcul de D après choix du pas, Andrei : test chaque élément

--- stage/LM.jl
+++ stage/LM.jl
@@ -1,157 +1,43 @@
 using LinearAlgebra, NLPModels, Printf, Logging, SolverCore, Test, ADNLPModels
 
-function LM(nlp          :: AbstractNLSModel; 
-                x        :: AbstractVector = nlp.meta.x0, 
-                ϵₐ       :: AbstractFloat = 10^(-8),
-                ϵᵣ       :: AbstractFloat = 10^(-8),
-                η₁       :: AbstractFloat = 1e-3, 
-                η₂       :: AbstractFloat = 0.66, 
-                σ₁       :: AbstractFloat = 10.0, 
-                σ₂       :: AbstractFloat = 0.5,
-                max_eval :: Int = 1000, 
-                max_time :: AbstractFloat = 60.,
-                max_iter :: Int = typemax(Int64)
-                )
-
-    ################ On évalue F(x₀) et J(x₀) ################
-    Fx = residual(nlp, x)
-    Jx = jac_residual(nlp, x)
-    Gx = Jx' * Fx
-
-    ################## On calcule leur norme #################
-    normFx₀   = norm(Fx)
-    normGx₀ = norm(Gx)
-    normGx = normGx₀
-    normFx = normFx₀
-
-    iter = 0    
-    λ = 0.0
-    λ₀ = 1e-6
-
-    iter_time = 0.0
-    tired   = neval_residual(nlp) > max_eval || iter_time > max_time
-    status  = :unknown
-    start_time = time()
-    optimal    = normGx ≤ ϵₐ + ϵᵣ*normGx₀ || normFx ≤ ϵₐ + ϵᵣ*normFx₀
-
-    #################### Tracé des graphes ###################
-
-    objectif = [normFx]
-    gradient = [normGx]
-
-    @info log_header(
-        [:iter, :nf, :obj, :grad, :status, :nd, :λ],
-        [Int, Int, Float64, Float64, String, Float64, Float64],
-        hdr_override=Dict(
-            :nf => "#F", :obj => "‖F(x)‖", :grad => "‖J'.F‖", :nd => "‖d‖", :λ => "λ")
-        )
-
-    while !(optimal || tired)
-        ################# Calcul de d par factorisation QR #################
-        n = size(Jx, 2)
-        A = [Jx; λ * I(n)]
-        b = [Fx; zeros(n)]
-        
-        QR = qr(A)
-        d = QR\(-b)
-
-        ######################## Calcul du ratio ρ ########################
-        xp      = x + d
-        Fxp     = residual(nlp, xp)
-        normFxp = norm(Fxp)
-
-        ρ = (normFx^2 - normFxp^2) / (normFx^2 - norm(Jx * d + Fx)^2)
-        
-        if ρ < η₁
-            λ = max(λ₀, σ₁ * λ)
-            status = :increase_λ
-        else
-            x   = xp
-            Fx  = residual(nlp, x)
-            Jx  = jac_residual(nlp, x)
-            normFx   = norm(Fx)
-            normGx = norm(Jx' * Fx)
-            status = :success
-            if ρ ≥ η₂
-                λ = σ₂ * λ
-            end
-        end
-
-        push!(objectif,normFx)
-        push!(gradient, normGx)
-
-        @info log_row(Any[iter, neval_residual(nlp), normFx, normGx, status, norm(d), λ])
-
-        iter_time    = time() - start_time
-        iter        += 1
-
-        many_evals   = neval_residual(nlp) > max_eval
-        iter_limit   = iter > max_iter
-        tired        = many_evals || iter_time > max_time || iter_limit
-        optimal      = min(normFx, normGx) ≤ ϵₐ + ϵᵣ*normGx₀
-    end
-
-
-    status = if optimal 
-        :first_order
-        elseif tired
-            if neval_residual(nlp) > max_eval
-                :max_eval
-            elseif iter_time > max_time
-                :max_time
-            elseif iter > max_iter
-                :max_iter
-            else
-                :unknown_tired
-            end
-        else
-            :unknown
-        end
-    
-    return GenericExecutionStats(nlp; 
-                    status, 
-                    solution = x,
-                    objective = normFx^2 / 2,
-                    dual_feas = normGx,
-                    iter = iter, 
-                    elapsed_time = iter_time)#, objectif, gradient
-end
-
 
 function LM_D(nlp        :: AbstractNLSModel;
     fctDk    :: Function =  Andrei,
-    x        :: AbstractVector = nlp.meta.x0, 
+    x0       :: AbstractVector = nlp.meta.x0, 
     ϵₐ       :: AbstractFloat = 10^(-8),
     ϵᵣ       :: AbstractFloat = 10^(-8),
     η₁       :: AbstractFloat = 1e-3, 
     η₂       :: AbstractFloat = 0.66, 
     σ₁       :: AbstractFloat = 10.0, 
     σ₂       :: AbstractFloat = 0.5,
+    ApproxD  :: Bool = true,
     max_eval :: Int = 1000, 
     max_time :: AbstractFloat = 60.,
     max_iter :: Int = typemax(Int64)
     )
 
     ################ On évalue F(x₀) et J(x₀) ################
+    x = copy(x0)
     Fx = residual(nlp, x)
     Jx = jac_residual(nlp, x)
     Gx = Jx' * Fx
 
     m,n = size(Jx)
 
-    x₋₁    = zeros(n)
-    Fx₋₁   = zeros(m)
-    φk₋₁   = zeros(n)
     yk₋₁   = zeros(n)
     sk₋₁   = zeros(n)
-    Dk     = I(n)
-
+    
+    if ApproxD
+        Dk = I(n)
+    else
+        Dk = zeros(n,n)
+    end
 
     ################## On calcule leur norme #################
-    normFx₀   = norm(Fx)
+    normFx₀ = norm(Fx)
     normGx₀ = norm(Gx)
-    normGx = normGx₀
-    normFx = normFx₀
+    normGx  = normGx₀
+    normFx  = normFx₀
 
     iter = 0    
     λ = 0.0
@@ -175,13 +61,6 @@ function LM_D(nlp        :: AbstractNLSModel;
         )
 
     while !(optimal || tired)
-        ########################### Calcul de D ############################
-        yk₋₁ = Jx' * Fx₋₁ - φk₋₁
-        sk₋₁ = x - x₋₁
-        
-        Dk = fctDk(Dk, sk₋₁, yk₋₁)
-            
-
         ################# Calcul de d par factorisation QR #################
         A = [Jx; (Dk + λ * I(n))^(1/2)]
         b = [Fx; zeros(n)]
@@ -194,7 +73,7 @@ function LM_D(nlp        :: AbstractNLSModel;
         Fxp     = residual(nlp, xp)
         normFxp = norm(Fxp)
 
-        ρ = (normFx^2 - normFxp^2) / (normFx^2 - norm(Jx * d + Fx)^2)
+        ρ = (normFx^2 - normFxp^2) / (normFx^2 - norm(Jx * d + Fx)^2 - d'*Dk*d)
 
         if ρ < η₁
             λ = max(λ₀, σ₁ * λ)
@@ -205,7 +84,6 @@ function LM_D(nlp        :: AbstractNLSModel;
             Fx₋₁ = Fx
             φk₋₁ = Gx
 
-
             ######################## Mise à jour #########################
             x    = xp
             Fx   = Fxp
@@ -213,6 +91,15 @@ function LM_D(nlp        :: AbstractNLSModel;
             Gx   = Jx' * Fx
             normFx   = norm(Fx)
             normGx = norm(Gx)
+
+            ######################## Calcul de D #########################
+            yk₋₁ = Jx' * Fx₋₁ - φk₋₁
+            sk₋₁ = x - x₋₁
+
+            if ApproxD
+                Dk = fctDk(Dk, sk₋₁, yk₋₁)
+            end
+
             status = :success    
             if ρ ≥ η₂
                 λ = σ₂ * λ
@@ -232,7 +119,6 @@ function LM_D(nlp        :: AbstractNLSModel;
         tired        = many_evals || iter_time > max_time || iter_limit
         optimal      = min(normFx, normGx) ≤ ϵₐ + ϵᵣ*normGx₀
     end
-
 
     status = if optimal 
         :first_order
@@ -256,7 +142,7 @@ function LM_D(nlp        :: AbstractNLSModel;
             objective = normFx^2 / 2,
             dual_feas = normGx,
             iter = iter, 
-            elapsed_time = iter_time)#, objectif, gradient
+            elapsed_time = iter_time), objectif, gradient
 end
 
 
@@ -265,7 +151,7 @@ function SPG(Dk₋₁, sk₋₁, yk₋₁)
     n   = size(Dk₋₁)[1]
     
     sty = sk₋₁' * yk₋₁
-    if sty > 0 
+    if sty > 0.01
         σk  = sty /(sk₋₁' * sk₋₁)
         Dk  = σk * I(n)
         return Dk
@@ -279,7 +165,7 @@ function Zhu(Dk₋₁, sk₋₁, yk₋₁)
     tr   = sum(Sk₋₁^4)
 
     frac  = sk₋₁'*(yk₋₁ - Dk₋₁ * sk₋₁)/tr
-    if frac > 0
+    if frac > 0.01
         Dk   = Dk₋₁ + frac * (Sk₋₁^2)
         return Dk
     else
@@ -287,17 +173,36 @@ function Zhu(Dk₋₁, sk₋₁, yk₋₁)
     end
 end
 
-function Andrei(Dk₋₁, sk₋₁, yk₋₁)
-    n    = size(Dk₋₁)[1]
-    Sk₋₁ = diagm(sk₋₁)
-    min  = minimum(sk₋₁)^2
-    tr   = sum(Sk₋₁^4)
+# function Andrei(Dk₋₁, sk₋₁, yk₋₁)
+#     n    = size(Dk₋₁)[1]
+#     Sk₋₁ = diagm(sk₋₁)
+#     min  = minimum(sk₋₁)^2
+#     tr   = sum(Sk₋₁^4)
 
+#     frac = sk₋₁'*(yk₋₁ + sk₋₁ - Dk₋₁ * sk₋₁)/tr
+#     if frac * min > 1.01
+#         Dk   = Dk₋₁ + frac * (Sk₋₁^2) - I(n)
+#         @show min(diag(Dk))
+#         return Dk
+#     else
+#         return Dk₋₁
+#     end
+# end 
+
+
+function Andrei(Dk₋₁, sk₋₁, yk₋₁; ϵ = 0.01)
+    n    = size(Dk₋₁)[1]
+    tr   = sum(sk₋₁ .^ 4)
     frac = sk₋₁'*(yk₋₁ + sk₋₁ - Dk₋₁ * sk₋₁)/tr
-    if frac * min > 1
-        Dk   = Dk₋₁ + frac * (Sk₋₁^2) - I(n)
-        return Dk
-    else
-        return Dk₋₁
+    Dk   = zeros(n,n)
+
+    for i = 1:n
+        Di = Dk₋₁[i,i] + frac * sk₋₁[i]^2 - 1
+        if Di > ϵ
+            Dk[i,i] = Di
+        else
+            Dk[i,i] = 1
+        end
     end
+    return Dk
 end 
--- stage/main.jl
+++ stage/main.jl
@@ -13,40 +13,52 @@ include("LM.jl")
 
 ###################### Test sur un problème unique #######################
 
-# problems_names = setdiff(names(NLSProblems), [:NLSProblems])
-# problems = (eval((problem))() for problem ∈ problems_names)
-# pb = collect(problems)
-# pb_sc = filter(problem -> problem.meta.ncon == 0, pb)
+problems_names = setdiff(names(NLSProblems), [:NLSProblems])
+problems = (eval((problem))() for problem ∈ problems_names)
+pb = collect(problems)
+pb_sc = filter(problem -> problem.meta.ncon == 0, pb)
+pb_test1 = filter(problem -> problem.meta.name == "tp272", pb)
 
+n = 15
+pb_test = pb_sc[n]
+pb_testD = pb_sc[n]
+pb_testZ = pb_sc[n]
+pb_testA = pb_sc[n]
 
-# pb_test1 = pb_sc[1]
-# pb_test2 = pb_sc[7]
-
-
-# stats, obj, grad = LM(pb_test2)
-# rangs = 1:length(grad)
-# stats_D, obj_D, grad_D = LM_D(pb_test2)
+stats, obj, grad = LM_D(pb_test1[1]; ApproxD = false)
+rangs = 1:length(grad)
+# stats_D, obj_D, grad_D = LM_D(pb_test1[1]; fctDk = SPG)
 # rangs_D = 1:length(grad_D)
+# stats_Z, obj_Z, grad_Z = LM_D(pb_test1[1]; fctDk = Zhu)
+# rangs_Z = 1:length(grad_Z)
+stats_A, obj_A, grad_A = LM_D(pb_test1[1]; fctDk = Andrei)
+rangs_A = 1:length(grad_A)
 
-# plot(rangs, grad, xlabel="k", ylabel="‖J‖",yaxis =:log10, label="LM classique")
-# plot!(rangs_D, grad_D, label="LM avec D")
-#savefig("obj_mgh03.png")
+plot(rangs, grad, xlabel="k", ylabel="‖JᵀF‖",yaxis =:log10, label="LM classique")
+# plot!(rangs_D, grad_D, label="LM avec D (SPG)")
+# plot!(rangs_Z, grad_Z, label="LM avec D (Z)")
+plot!(rangs_A, grad_A, label="LM avec D (A)")
+# savefig("obj_mgh03.png")
 
 
 ######################## Profils de performance #########################
 
 
-problems_names = setdiff(names(NLSProblems), [:NLSProblems])
-problems = (eval((problem))() for problem ∈ problems_names)
+# problems_names = setdiff(names(NLSProblems), [:NLSProblems])
+# problems = (eval((problem))() for problem ∈ problems_names)
 
-solvers = Dict(:LM => LM, :LM_D => LM_D)
-stats = bmark_solvers(solvers, problems)
+# solvers = Dict(
+#   :LM => model -> LM_D(model; ApproxD = false),
+#   :LM_SPG => model -> LM_D(model; fctDk = SPG),
+#   :LM_Andrei => model -> LM_D(model; fctDk = Andrei)
+# )
+# stats = bmark_solvers(solvers, problems)
 
-cols = [:name, :status, :objective, :elapsed_time, :iter]
-for solver ∈ keys(solvers)
-  pretty_stats(stats[solver][!, cols])
-end
-cost(df) = (df.status .!= :first_order) * Inf + df.iter
-performance_profile(stats, cost)
+# cols = [:name, :status, :objective, :elapsed_time, :iter]
+# for solver ∈ keys(solvers)
+#   pretty_stats(stats[solver][!, cols])
+# end
+# cost(df) = (df.status .!= :first_order) * Inf + df.iter
+# performance_profile(stats, cost)
 
-#savefig("performance_profile.png")
+# # #savefig("performance_profile.png")
